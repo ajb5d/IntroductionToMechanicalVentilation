769fe727f37601e7d977cbacbfd70cb84c1d07ba
Andrew Barros <ajb5d@virginia.edu>
2019-08-27 21:00:36 -0400

    Wording changes New figure for pressure/flow curves

---
+++ img/make_pressure_time.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(20.0, 5.0))
+
+ax1 = plt.subplot(1,2,1)
+ax1.set(xlim = (0,6), ylim = (0,30))
+
+ax2 = plt.subplot(1,2,2)
+ax2.set(xlim = (0,6), ylim = (-60,60))
+ax2.axhline(0, color = "black")
+
+fig.savefig("pressure_time.png")

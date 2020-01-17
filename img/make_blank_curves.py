c0a4e5795445b09bb7834cefcd8cc16397f8c319
Andrew Barros <ajb5d@virginia.edu>
2020-01-17 10:39:19 -0500

    Added lession 3

--- img/make_blank_curves.py
+++ img/make_blank_curves.py
@@ -10,3 +10,9 @@ ax2.set(xlim = (0,6), ylim = (-60,60))
 ax2.axhline(0, color = "black")
 
 fig.savefig("pressure_time.png")
+
+fig = plt.figure(figsize=(20.0, 5.0))
+
+ax1 = plt.subplot(1,1,1)
+ax1.set(xlim = (0,6), ylim = (0,500))
+fig.savefig("volume_time.png")
